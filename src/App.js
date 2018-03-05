8acf898ba26921b560c7ca33da11f0f59d099b08
cao <1920294731@qq.com>
2018-03-06 00:57:58 +0800

    完成书籍转移功能

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css'
 import {Link,Route} from 'react-router-dom'
 class BookInfor extends React.Component{
   constructor(props) {
-    console.log()
     super(props);
     this.hand =this.hand.bind(this);
     this.state={
@@ -12,9 +11,12 @@ class BookInfor extends React.Component{
     }
   }
   componentDidMount(){
-    console.log(this.props.book)
+
   }
   hand(e){
+    this.setState({
+      bookState:''
+    });
     this.setState({
       bookState:e.target.value
     });
@@ -48,7 +50,7 @@ class BookList extends React.Component{
     let self=this;
     let BookDom=[];
     this.props.bookReading.map(function (val,index) {
-      BookDom.push(<li><BookInfor book={val} moveBook={self.props.moveBook} /></li>)
+      BookDom.push(<li key={val.id}><BookInfor book={val} moveBook={self.props.moveBook} /></li>)
     });
     return (
       <ol className="books-grid">
@@ -59,19 +61,6 @@ class BookList extends React.Component{
 }
 class BookMain extends React.Component{
   state = {
-    reading:[
-      'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-      'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
-    ],
-    toRead:[
-      'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
-      'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")'
-    ],
-    read:[
-      'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")',
-      'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")',
-      'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")'
-    ],
     bookList: [
       {
         url:'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
@@ -83,7 +72,6 @@ class BookMain extends React.Component{
       {
         url:'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
         states:'reading',
-        //todo 单引号的转义符号
         title:'Ender\'s Game',
         authors:'Orson Scott Card',
         id:2
@@ -98,7 +86,6 @@ class BookMain extends React.Component{
       {
         url:'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")',
         states:'toRead',
-        //todo 转义字符
         title:'Harry Potter and the Sorcerer\'s Stone',
         authors:'J.K. Rowling',
         id:4
@@ -113,7 +100,6 @@ class BookMain extends React.Component{
       {
         url:'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")',
         states:'read',
-        //todo zhuanyi
         title:'Oh, the Places You\'ll Go!',
         authors:'Seuss',
         id:6
@@ -125,7 +111,7 @@ class BookMain extends React.Component{
         authors:'Mark Twain',
         id:7
       },
-    ]
+    ],
   };
   componentDidMount(){
     let self=this;
@@ -133,34 +119,52 @@ class BookMain extends React.Component{
   moveBook(val,toState){
     let self=this;
     let curremtBookIndex;
-    console.log(toState)
     this.state.bookList.forEach(function (value,index) {
       if (val.id===value.id){
         curremtBookIndex=index;
       }
     });
-    let  newBookList;
+    // let  newBookList;
     switch (toState){
       case "currentlyReading":
-        this.state.bookList[curremtBookIndex].states="currentlyReading";
-        newBookList=this.state.bookList.filter(()=>true);//复制新的数组
-        this.setState({
-          bookList: newBookList
+        console.log("currentlyReading");
+        this.setState(function (prevState,props) {
+          prevState.bookList[curremtBookIndex].states="reading";
+          let  newBookList=prevState.bookList.filter(()=>true)
+          return {
+            bookList: newBookList
+          };
         });
         break;
       case "wantToRead":
-        this.state.bookList[curremtBookIndex].states="wantToRead";
-        newBookList=this.state.bookList.filter(()=>true);//复制新的数组
-        this.setState({
-          bookList:newBookList
+        console.log("wantToRead");
+        this.setState(function (prevState,props) {
+          prevState.bookList[curremtBookIndex].states="toRead";
+          let  newBookList=prevState.bookList.filter(()=>true)
+          return {
+            bookList: newBookList
+          };
         });
+        // this.state.bookList[curremtBookIndex].states="wantToRead";
+        // newBookList=this.state.bookList.filter(()=>true);//复制新的数组
+        // this.setState({
+        //   bookList:newBookList
+        // });
         break;
       case "read":
-        this.state.bookList[curremtBookIndex].states="read";
-        newBookList=this.state.bookList.filter(()=>true);//复制新的数组
-        this.setState({
-          bookList:newBookList
+        console.log("read");
+        this.setState(function (prevState,props) {
+          prevState.bookList[curremtBookIndex].states="read";
+          let  newBookList=prevState.bookList.filter(()=>true)
+          return {
+            bookList: newBookList
+          };
         });
+        // this.state.bookList[curremtBookIndex].states="read";
+        // newBookList=this.state.bookList.filter(()=>true);//复制新的数组
+        // this.setState({
+        //   bookList:newBookList
+        // });
         break;
       default:break;
     }
