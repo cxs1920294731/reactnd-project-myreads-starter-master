53d500ec7c49c9229ec8c4d79a557c45df407a2a
cao <1920294731@qq.com>
2018-03-05 23:17:40 +0800

    bug

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import {Link,Route} from 'react-router-dom'
-// http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api
 class BookInfor extends React.Component{
-  state={
-      index: this.props.index
+  constructor(props) {
+    console.log()
+    super(props);
+    this.hand =this.hand.bind(this);
+    this.state={
+      bookState: this.props.book.states
+    }
+  }
+  componentDidMount(){
+    console.log(this.props.book)
   }
   hand(e){
     this.setState({
-      index:e.target.value
+      bookState:e.target.value
     });
-    this.props.moveBook(this.props.index,e.target.value)
+    this.props.moveBook(this.props.book,e.target.value)
   }
   render(){
     return(
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: this.props.url }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: this.props.book.url }}></div>
           <div className="book-shelf-changer">
-            <select value={this.state.index}  onChange={this.hand.bind(this)}>
+            <select value={this.state.bookState}  onChange={this.hand}>
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
@@ -28,8 +35,8 @@ class BookInfor extends React.Component{
             </select>
           </div>
         </div>
-        <div className="book-title">To Kill a Mockingbird</div>
-        <div className="book-authors">Harper Lee</div>
+        <div className="book-title">{this.props.book.title}</div>
+        <div className="book-authors">{this.props.book.authors}</div>
       </div>
     );
   }
@@ -41,7 +48,7 @@ class BookList extends React.Component{
     let self=this;
     let BookDom=[];
     this.props.bookReading.map(function (val,index) {
-      BookDom.push(<li><BookInfor index={self.props.index} moveBook={self.props.moveBook} url={val}/></li>)
+      BookDom.push(<li><BookInfor book={val} moveBook={self.props.moveBook} /></li>)
     });
     return (
       <ol className="books-grid">
@@ -69,80 +76,92 @@ class BookMain extends React.Component{
       {
         url:'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
         states:'reading',
-        title:'',
-        authors:''
+        title:'To Kill a Mockingbird',
+        authors:'Harper Lee',
+        id:1
       },
       {
         url:'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
         states:'reading',
-        title:'',
-        authors:''
+        //todo 单引号的转义符号
+        title:'Ender\'s Game',
+        authors:'Orson Scott Card',
+        id:2
       },
       {
         url:'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
         states:'toRead',
-        title:'',
-        authors:''
+        title:'1776',
+        authors:'David McCullough',
+        id:3
       },
       {
         url:'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")',
         states:'toRead',
-        title:'',
-        authors:''
+        //todo 转义字符
+        title:'Harry Potter and the Sorcerer\'s Stone',
+        authors:'J.K. Rowling',
+        id:4
       },
       {
         url:'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")',
         states:'read',
-        title:'',
-        authors:''
+        title:'The Hobbit',
+        authors:'J.R.R. Tolkien',
+        id:5
       },
       {
         url:'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")',
         states:'read',
-        title:'',
-        authors:''
+        //todo zhuanyi
+        title:'Oh, the Places You\'ll Go!',
+        authors:'Seuss',
+        id:6
       },
       {
         url:'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")',
         states:'read',
-        title:'',
-        authors:''
+        title:'The Adventures of Tom Sawyer',
+        authors:'Mark Twain',
+        id:7
       },
     ]
   };
   componentDidMount(){
     let self=this;
   }
-
-  moveToState(book,toState){
-    switch (toState){
-      case "currentlyReading":
-        this.setState({
-
-        })
-        break;
-      case "wantToRead":
-        this.setState({
-          toRead: this.state.toRead.push(book)
-        })
-        break;
-      case "read": break;
-      default:break;
-    }
-  }
   moveBook(val,toState){
     let self=this;
-    console.log();
-    console.log(toState);
-    switch (val){
+    let curremtBookIndex;
+    console.log(toState)
+    this.state.bookList.forEach(function (value,index) {
+      if (val.id===value.id){
+        curremtBookIndex=index;
+      }
+    });
+    let  newBookList;
+    switch (toState){
       case "currentlyReading":
-        console.log("cao");
-        let index=this.state.reading.indexOf(val);
-        let book=this.state.reading.splice(index,1)[0];
-        this.moveToState(book,toState)
+        this.state.bookList[curremtBookIndex].states="currentlyReading";
+        newBookList=this.state.bookList.filter(()=>true);//复制新的数组
+        this.setState({
+          bookList: newBookList
+        });
         break;
-      case "wantToRead": break;
-      case "read": break;
+      case "wantToRead":
+        this.state.bookList[curremtBookIndex].states="wantToRead";
+        newBookList=this.state.bookList.filter(()=>true);//复制新的数组
+        this.setState({
+          bookList:newBookList
+        });
+        break;
+      case "read":
+        this.state.bookList[curremtBookIndex].states="read";
+        newBookList=this.state.bookList.filter(()=>true);//复制新的数组
+        this.setState({
+          bookList:newBookList
+        });
+        break;
       default:break;
     }
   }
@@ -158,8 +177,7 @@ class BookMain extends React.Component{
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <BookList index="currentlyReading" bookReading= {this.state.bookList.filter(function (val) {
-                  if(val.states=='reading') return true;
-                  else return false;
+                  return (val.states=='reading') ? true : false;
                 })} moveBook={this.moveBook.bind(this)} />
               </div>
             </div>
@@ -167,18 +185,16 @@ class BookMain extends React.Component{
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
                 <BookList index="wantToRead" bookReading= {this.state.bookList.filter(function (val) {
-                  if(val.states=='toRead') return true;
-                  else return false;
-                })} moveBook={this.moveBook} />
+                   return (val.states=='toRead') ? true : false;
+                })} moveBook={this.moveBook.bind(this)} />
               </div>
             </div>
             <div className="bookshelf">
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <BookList index="read" bookReading= {this.state.bookList.filter(function (val) {
-                  if(val.states=='read') return true;
-                  else return false;
-                })} moveBook={this.moveBook} />
+                  return (val.states=='read') ? true:false;
+                })} moveBook={this.moveBook.bind(this)} />
               </div>
             </div>
           </div>
