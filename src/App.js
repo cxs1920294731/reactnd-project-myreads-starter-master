8f430868f541877cbaaf7b160c30f98365c9b0b4
Bencao <1920294731@qq.com>
2018-03-05 19:38:12 +0800

    初始化book对象

--- src/App.js
+++ src/App.js
@@ -40,7 +40,6 @@ class BookList extends React.Component{
   render(){
     let self=this;
     let BookDom=[];
-    console.log('43',this.props.bookReadings)
     this.props.bookReading.map(function (val,index) {
       BookDom.push(<li><BookInfor index={self.props.index} moveBook={self.props.moveBook} url={val}/></li>)
     });
@@ -52,7 +51,7 @@ class BookList extends React.Component{
   }
 }
 class BookMain extends React.Component{
-  state={
+  state = {
     reading:[
       'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
       'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
@@ -66,57 +65,55 @@ class BookMain extends React.Component{
       'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")',
       'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")'
     ],
-    bookList:[
-      this.bookClass({
-        url:this.reading,
+    bookList: [
+      {
+        url:'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
         states:'reading',
         title:'',
         authors:''
-      }),
-      this.bookClass({
-        url:this.reading[1],
+      },
+      {
+        url:'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
         states:'reading',
         title:'',
         authors:''
-      }),
-      this.bookClass({
-        url:this.toRead[0],
+      },
+      {
+        url:'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
         states:'toRead',
         title:'',
         authors:''
-      }),
-      this.bookClass({
-        url:this.toRead[1],
+      },
+      {
+        url:'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")',
         states:'toRead',
         title:'',
         authors:''
-      }),
-      this.bookClass({
-        url:this.read[0],
+      },
+      {
+        url:'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")',
         states:'read',
         title:'',
         authors:''
-      }),
-      this.bookClass({
-        url:this.read[1],
+      },
+      {
+        url:'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")',
         states:'read',
         title:'',
         authors:''
-      }),
-      this.bookClass({
-        url:this.read[2],
+      },
+      {
+        url:'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")',
         states:'read',
         title:'',
         authors:''
-      }),
+      },
     ]
   };
-  bookClass({url,states,title,authors}){
-    this.url=url;
-    this.states=states;
-    this.title=title;
-    this.authors=authors;
+  componentDidMount(){
+    let self=this;
   }
+
   moveToState(book,toState){
     switch (toState){
       case "currentlyReading":
@@ -152,7 +149,6 @@ class BookMain extends React.Component{
   render(){
     return(
       <div className="list-books">
-        <div>{this.state.read[0]}</div>
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
@@ -161,19 +157,28 @@ class BookMain extends React.Component{
             <div className="bookshelf">
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
-                <BookList index="currentlyReading" bookReading= {this.state.reading} moveBook={this.moveBook.bind(this)} />
+                <BookList index="currentlyReading" bookReading= {this.state.bookList.filter(function (val) {
+                  if(val.states=='reading') return true;
+                  else return false;
+                })} moveBook={this.moveBook.bind(this)} />
               </div>
             </div>
             <div className="bookshelf">
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
-                <BookList index="wantToRead" bookReading= {this.state.toRead} moveBook={this.moveBook} />
+                <BookList index="wantToRead" bookReading= {this.state.bookList.filter(function (val) {
+                  if(val.states=='toRead') return true;
+                  else return false;
+                })} moveBook={this.moveBook} />
               </div>
             </div>
             <div className="bookshelf">
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
-                <BookList index="read" bookReading= {this.state.read} moveBook={this.moveBook} />
+                <BookList index="read" bookReading= {this.state.bookList.filter(function (val) {
+                  if(val.states=='read') return true;
+                  else return false;
+                })} moveBook={this.moveBook} />
               </div>
             </div>
           </div>
