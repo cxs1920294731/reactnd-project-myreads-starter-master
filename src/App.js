80f94b2f44bf90d1c2171755aaac38de88d5bc8b
cao <1920294731@qq.com>
2018-03-08 23:19:24 +0800

    修改获取数据的路径

--- src/App.js
+++ src/App.js
@@ -8,16 +8,44 @@ import './App.css'
 class BooksApp extends React.Component {
   constructor(props){
     super(props)
-    SessionApi.ininSessionStorage();
+    this.state={
+      booklist:[]
+    };
+    this.moveBook = this.moveBook.bind(this);
   }
   componentDidMount(){
-    BooksAPI.getAll().then((data)=>{console.log(data)})
+    BooksAPI.getAll().then((data)=>{
+      let self=this;
+      console.log(data)
+      self.setState({
+        booklist:data
+      })
+    })
+  }
+  moveBook(book,toState){
+    let self=this;
+    BooksAPI.update(book,toState).then((bookIDList)=>{
+      console.log(bookIDList);
+
+      self.setState({
+        booklist:self.state.booklist.map((value)=>{
+          if(bookIDList.currentlyReading.indexOf(value.id)>-1){
+             value.shelf='currentlyReading';
+          }else if(bookIDList.wantToRead.indexOf(value.id)>-1){
+            value.shelf='wantToRead';
+          }else  if(bookIDList.read.indexOf(value.id)>-1){
+            value.shelf='read';
+          }
+          return value;
+        })
+      })
+    });
   }
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={BookMain}></Route>
-        <Route exact path="/search" component={Search}></Route>
+        <Route exact path="/" render={()=>(<BookMain bookList={this.state.booklist} moveBook={this.moveBook}/>)}/>
+        <Route exact path="/search" render={()=>(<Search bookList={this.state.booklist} moveBook={this.moveBook}/>)}/>
       </div>
     )
   }
